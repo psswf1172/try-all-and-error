3fb14e9b76c93e1c83b7e7167173be81eb066771
psswf1172 <jennifermabella@gmail.com>
2016-06-16 18:23:18 -0400

    initial commit

---
+++ HTML/php/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+// SMTP Server Settings
+$config['smtp_host'] 			= 'ssl://smtp.gmail.com';   // eg.: smtp.mandrillapp.com
+$config['smtp_port'] 			= 465;						// eg.: 587
+$config['smtp_user'] 			= 'jennifermabella@gmail.com'; 						// you@gmail.com
+$config['smtp_pass'] 			= '11Princess';						// password
+$config['smtp_ssl']				= false;					// should remain false
+
+// Who receive all emails?
+$config['send_to']				= 'jennifermabella@gmail.com';			// destination of all emails sent throught contact form
+
+// Email Subject
+$config['subject']				= 'Email from your personal website!';			// subject of emails you receive
+?>
